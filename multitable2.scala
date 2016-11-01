8b5a5b51d3e6e2e6c1b2201759ea9a1a97056b9f
Niels Koster <n.koster@portavita.eu>
2016-11-01 22:38:03 +0100

    Table should be String here (wrong in book)

--- multitable2.scala
+++ multitable2.scala
@@ -2,7 +2,7 @@ def printMultiTable() {
   for (i <- 1 to 10) {
     for (j <- 1 to 10) {
       val prod = (i * j).toString
-      print(String.format("%4s", Array(prod)))
+      print(String.format("%4s", prod))
     }
     println()
   }
