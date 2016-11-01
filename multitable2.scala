83b860b8063422c5d29061d31abe455224d2ee2c
Niels Koster <n.koster@portavita.eu>
2016-11-01 22:35:14 +0100

    Multitable in more functional way

---
+++ multitable2.scala
@@ -0,0 +1,11 @@
+def printMultiTable() {
+  for (i <- 1 to 10) {
+    for (j <- 1 to 10) {
+      val prod = (i * j).toString
+      print(String.format("%4s", Array(prod)))
+    }
+    println()
+  }
+}
+
+printMultiTable()
