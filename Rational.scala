19b262e831e9b44ac225be5959cb0115ffa06c66
Niels Koster <n.koster@portavita.eu>
2016-11-05 19:58:08 +0100

    Rational class

---
+++ Rational.scala
@@ -0,0 +1,26 @@
+class Rational(n: Int, d: Int) {
+
+  require(d != 0)
+
+  val numer: Int = n
+  val denom: Int = d
+
+  override def toString = numer + "/" + denom
+
+  def add(r: Rational): Rational = {
+    new Rational(
+      numer * r.denom + r.numer * denom,
+      denom * r.denom
+    )
+  }
+
+  def lessThan(r: Rational) = {
+    // this.numer * r.denom < r.numer * this.denom
+    numer * r.denom < r.numer * denom
+  }
+
+  def max(r: Rational): Rational =
+    // if (this.lessThan(r)) r else this
+    if (lessThan(r)) r else this
+
+}
