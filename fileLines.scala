345c3d3653780245c8f2aff6b033ddca51c1ecd0
Niels Koster <n.koster@portavita.eu>
2016-11-06 00:55:50 +0100

    Mid-stream assignment in for loop

--- fileLines.scala
+++ fileLines.scala
@@ -8,7 +8,8 @@ def grep(pattern: String) =
     file <- filesHere
     if file.getName.endsWith(".scala")
     line <- fileLines(file)
-    if line.trim.matches(pattern)
-  } println(file + ": " + line.trim)
+    trimmed = line.trim
+    if trimmed.matches(pattern)
+  } println(file + ": " + trimmed)
 
 grep(".*gcd.*")
