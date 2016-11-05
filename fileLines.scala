856d90d8b3d237eced4202fb0c0e5756a922bf5f
Niels Koster <n.koster@portavita.eu>
2016-11-06 00:50:34 +0100

    Curly braces, no semicolon

--- fileLines.scala
+++ fileLines.scala
@@ -4,11 +4,11 @@ def fileLines(file: java.io.File) =
   scala.io.Source.fromFile(file).getLines().toList
 
 def grep(pattern: String) =
-  for (
+  for {
     file <- filesHere
-    if file.getName.endsWith(".scala");
+    if file.getName.endsWith(".scala")
     line <- fileLines(file)
     if line.trim.matches(pattern)
-  ) println(file + ": " + line.trim)
+  } println(file + ": " + line.trim)
 
 grep(".*gcd.*")
