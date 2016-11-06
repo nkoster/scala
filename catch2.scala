0437cbf6800e1a3fe7e5984615307dc88610263a
Niels Koster <n.koster@portavita.eu>
2016-11-07 00:41:23 +0100

    Another try catch

---
+++ catch2.scala
@@ -0,0 +1,10 @@
+import java.net.URL
+import java.net.MalformedURLException
+
+def urlFor(path: String) =
+  try {
+    new URL(path)
+  } catch {
+    case e: MalformedURLException =>
+      new URL("http://drone-existence.com")
+  }
