5abfe0972fe12f3a16b8a24bc7ed5c6f643709f5
Niels Koster <n.koster@portavita.eu>
2016-11-14 17:47:20 +0100

    Another example where tail optimization does work

--- recursion.scala
+++ recursion.scala
@@ -1,15 +1,26 @@
-/*
 // "head" recursion
 def boom(x: Int): Int =
   if (x == 0) throw new Exception("boom")
   else boom(x - 1) + 1
 
-boom(3)
-*/
+// boom(3)
 
 // "tail" recursion
 def bang(x: Int): Int =
   if (x == 0) throw new Exception("bang")
   else bang(x - 1)
 
-bang(5)
+// bang(5)
+
+def isEven(x: Int): Boolean =
+  if (x == 0) true else isOdd(x - 1)
+
+def isOdd(x: Int): Boolean =
+  if (x == 0) true else isEven(x - 1)
+
+val funValue = nestedFun _
+def nestedFun(x: Int): Unit = {
+  if (x != 0) { println(x); funValue(x - 1) }
+}
+
+nestedFun(10)
