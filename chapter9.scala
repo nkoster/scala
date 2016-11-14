c193e98df8e0de4413a76549d13d65d6f7912202
Niels Koster <n.koster@portavita.eu>
2016-11-14 23:36:06 +0100

    Add fileContaining method

--- chapter9.scala
+++ chapter9.scala
@@ -1,12 +1,23 @@
 object FileMatcher {
+
   private def filesHere = (new java.io.File(".")).listFiles
+
   def filesEnding(q: String) =
     for (
       file <-filesHere
       if (file.getName.endsWith(q))
     ) yield file
+
+  def filesContaining(q: String) =
+    for (
+      file <- filesHere
+      if (file.getName.contains(q))
+    ) yield file
+
 }
 
 val f = FileMatcher
 
 f.filesEnding(".scala").foreach(println)
+println("-----------")
+f.filesContaining("chap").foreach(println)
