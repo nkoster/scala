0c9503c976f1fd19893f9dbcc1cbc06d73007418
Niels Koster <n.koster@portavita.eu>
2016-11-18 16:41:10 +0100

    Even less code, can you follow (:

--- filesmatching.scala
+++ filesmatching.scala
@@ -17,18 +17,26 @@ object FileMatcher1 {
 }
 
 object FileMatcher2 {
+
   private def filesHere = (new java.io.File(".")).listFiles
 
-  def filesMatching(q: String,
-                    matcher: (String, String) => Boolean) = {
+  private def filesMatching(matcher: (String) => Boolean) = {
     for (
       file <- filesHere
-      if matcher(file.getName, q)
+      if matcher(file.getName)
     ) yield file
   }
 
-  def filesEnding(q: String) = filesMatching(q, _.endsWith(_))
-  def filesContaining(q: String) = filesMatching(q, _.contains(_))
-  def filesRegex(q: String) = filesMatching(q, _.matches(_))
+  def filesEnding(q: String) = filesMatching(_.endsWith(q))
+  def filesContaining(q: String) = filesMatching(_.contains(q))
+  def filesRegex(q: String) = filesMatching(_.matches(q))
 
 }
+
+val f = FileMatcher2
+
+f.filesEnding(".scala").foreach(println)
+println("-----------")
+f.filesContaining("chap").foreach(println)
+println("-----------")
+f.filesRegex("^[c].*$").foreach(println)
